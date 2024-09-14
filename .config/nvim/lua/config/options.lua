3337d0e17f15a8e3885ca67971988c29964bd95a
james@falconx.io <james@falconx.io>
2024-09-14 00:50:47 -0400

    update chatgpt and minor cleanups

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -32,6 +32,10 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
 -- Do I still want this?
 -- vim.opt.pumblend = 20
--- .config/nvim/lua/plugins/chatgpt.lua
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -7,8 +7,15 @@ return {
                 openai_params = {
                     model = "gpt-4o",
                     max_tokens = 4096,
+                },
+                edit_with_instructions = {
+                    keymaps = {
+                      use_output_as_input = "<C-p>",
+                    },
                 }
             })
+          vim.keymap.set({'n', 'v'}, '<leader>ce', "<cmd>ChatGPTEditWithInstructions<CR>", { desc = '[C]hatGPT[E]ditWithInstuctions' })
+          vim.keymap.set({'n', 'v'}, '<leader>cc', "<cmd>ChatGPT<CR>", { desc = '[C]hatGPT[C]' })
         end,
         dependencies = {
           "MunifTanjim/nui.nvim",
