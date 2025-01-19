7c88a5bbf414a706069d9930648b17ebf02fd3fd
james@falconx.io <james@falconx.io>
2025-01-19 12:50:23 -0500

    show hidden files for leader tf, continue to hide .git

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -54,7 +54,12 @@ return {
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>cs', builtin.colorscheme, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>tf', function()
+      builtin.find_files({
+        hidden = true,
+        file_ignore_patterns = { "%.git/" },
+      })
+    end, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ts', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set("n", "<leader>tp", function()
       builtin.live_grep {
