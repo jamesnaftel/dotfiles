7c711107986879aa34a9f80fc375bce285ab425c
james-falconx <james@falconx.io>
2026-06-09 10:00:58 -0400

    update treesitter, add aliases, tweaks

--- .config/nvim/lua/james/plugins/treesitter.lua
+++ .config/nvim/lua/james/plugins/treesitter.lua
@@ -1,45 +1,24 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
     enabled = true,
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua',
+    config = function()
+      require('nvim-treesitter').install({
+        'bash', 'c', 'diff', 'html', 'lua',
         'luadoc', 'markdown', 'markdown_inline',
         'query', 'vim', 'vimdoc', 'go', 'python',
         'rust', 'terraform', 'javascript', 'tsx',
-        'json', 'cpp', 'dockerfile', 'yaml', 'zig'
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-      },
-    },
-    config = function(_, opts)
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(vim.tbl_deep_extend("force", opts, {
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-        },
-      }))
+        'json', 'cpp', 'dockerfile', 'yaml', 'zig',
+      })
     end,
   },
   {
     'nvim-treesitter/nvim-treesitter-textobjects',
-    after = 'nvim-treesitter',
+    enabled = false,
   },
   {
     'nvim-treesitter/nvim-treesitter-context',
-    after = 'nvim-treesitter',
   }
 }
